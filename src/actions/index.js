1ce870e6310ee00236320934385277447795d978
Sachin Samal <88958992+Sacsam005@users.noreply.github.com>
2022-01-17 00:22:19 -0600

    add

---
+++ src/actions/index.js
@@ -0,0 +1,13 @@
+export const increment = () => {
+    return {
+        type: 'INCREMENT',
+        payload: '',
+    }
+}
+
+export const decrement = () => {
+    return {
+        type: 'DECREMENT',
+        payload: '',
+    }
+}
